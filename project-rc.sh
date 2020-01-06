c827cef97822e9ce265d8d9e89e61fac1174f18a
Alissa Huskey <alissa.huskey@gmail.com>
2020-01-06 06:35:18 -0700

    bugfixes
    - run on_exit even if there's no new envfile
    - add comments
    - move auth and is_auth logic to functions
    - define PROJECT_ENVFILE and PROJECT_ROOT only if authed

--- project-rc.sh
+++ project-rc.sh
@@ -6,6 +6,10 @@
 
 RC_FILENAMES=( .projectrc .project-env .project .env )
 
+# usage: project:find_up <filename> [...<filename>]
+#   walk up the directory tree until any <filename>
+#   is found, then print it
+#   if not found, return 1
 project:find_up() {
   (
     while true; do
@@ -25,10 +29,14 @@ project:find_up() {
   )
 }
 
+# usage: project:myshell
+#   print the current shell
 project:myshell() {
   ps -ocomm= -p $$
 }
 
+# usage: project:valof <varname>
+#   print the value of <varname>
 project:valof() {
   local v shell
 
@@ -44,6 +52,9 @@ project:valof() {
   printf "%s" "$v"
 }
 
+# usage: project:export <varname> <value>
+#   save the current value of <varname> to OLD_<varname>
+#   set <varname> to <value> then export it
 project:export() {
   local varname="${1}" old_varname="OLD_${1}" oldval newval="${2}"
 
@@ -53,6 +64,8 @@ project:export() {
   export ${varname}="${newval}"
 }
 
+# usage: project:revert <varname>
+#   set <varname> to the value of OLD_<varname> then export it
 project:revert() {
   local varname="${1}" old_varname="OLD_${1}"
 
@@ -60,10 +73,33 @@ project:revert() {
   unset "${old_varname}"
 }
 
+# usage: project:is_authed <envfile>
+#   returns true if <envfile> is authorized
+project:is_authed() {
+  test -f "${authfile}" && gpg --quiet --decrypt "${authfile}" | grep -xq "${1}"
+}
+
+# usage: auth <envfile>
+#    authorize <envfile>
+project:auth() {
+  if project:is_authed "${1}"; then
+    return
+  fi
+
+  {
+    gpg --quiet --decrypt "${authfile}" 2> /dev/null
+    echo "${1}"
+  } | gpg --quiet --yes --default-recipient-self --armor --encrypt --output "${authfile}"
+}
+
+# project:env:source <envfile>
+#   run on_exit, source <envfile>, and run on_enter
 project:env:source() {
   local envfile="${1}" PROJECT_ROOT="${1%/*}" confirm authfile
   authfile="${HOME}/.projectrc-auth"
 
+  # run the defined on_exit function from previously loaded envfile
+  # then clear on_exit and on_enter
   if declare -f on_exit > /dev/null; then
     on_exit
     unset -f on_exit
@@ -73,42 +109,53 @@ project:env:source() {
     unset -f on_enter
   fi
 
-  if [[ -f "${envfile}" ]]; then
-    if test -f "${authfile}" && \
-      gpg --quiet --decrypt "${authfile}" | grep -xq "${PROJECT_ROOT}"; then
-      confirm=y
-    else
-      printf "sourcing file: %s ; ok? " "${envfile}"
-      read -r confirm
-      echo
-    fi
-
-    if [[ "$confirm" == "y" ]] || [[ "$confirm" == "Y" ]]; then
-      source "${envfile}"
-      {
-        gpg --quiet --decrypt "${authfile}" 2> /dev/null
-        echo "${PROJECT_ROOT}"
-      } | gpg --quiet --yes --default-recipient-self --armor --encrypt --output "${authfile}"
-    else
-      echo "Not sourcing."
-    fi
+  # exit early without an envfile
+  if [[ ! -f "${envfile}" ]]; then
+    return
   fi
 
-  if declare -f on_enter > /dev/null; then
-    on_enter "${envfile%/*}"
-  fi
-}
+  # check if the envfile is authed
+  if project:is_authed "${envfile}"; then
+    confirm=y
 
-project:env:load() {
-  envfile="$(project:find_up "${RC_FILENAMES[@]}")"
-  if [[ "${envfile}" != "${PROJECT_ENVFILE}" ]]; then
+  # or prompt to auth
+  else
+    printf "sourcing file: %s ; ok? " "${envfile}"
+    read -r confirm
+    echo
+  fi
+
+  # auth and source envfile then run on_enter
+  if [[ "$confirm" == "y" ]] || [[ "$confirm" == "Y" ]]; then
     export PROJECT_ENVFILE="${envfile}"
     export PROJECT_ROOT="${envfile%/*}"
 
+    project:auth "${envfile}"
+    source "${envfile}"
+
+    if declare -f on_enter > /dev/null; then
+      on_enter "${envfile%/*}"
+    fi
+
+  # or refuse to authrozie
+  else
+    echo "Refusing to source."
+  fi
+}
+
+# usage: project:env:load
+#   run in the current directory to search for a project envfile,
+#   set relevant env variables and source if authed
+project:env:load() {
+  local envfile
+  envfile="$(project:find_up "${RC_FILENAMES[@]}")"
+  if [[ "${envfile}" != "${PROJECT_ENVFILE}" ]]; then
     project:env:source "${envfile}"
   fi
 }
 
+# usage: project:env:cd
+#   for aliasing cd to
 project:env:cd() {
   local envfile
 
